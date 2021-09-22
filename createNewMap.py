87158895554bd9b233cfbce041a2ce2d9cf2927e
Alexandre <alexandre.vauthier@edu.devinci.fr>
2021-09-22 11:42:37 +0200

    mapInit bug fix

--- createNewMap.py
+++ createNewMap.py
@@ -12,10 +12,10 @@ map = []
 variables = Variables()
 
 def mapInit() :
-    for i in range(variables.mapSize + 1) :
-        for j in range(variables.mapSize + 1) :
+    for i in range(variables.mapSize) :
+        for j in range(variables.mapSize) :
             # if random.randint(1, 4) != 1 :
-            pygame.draw.rect(ecran, (218, 227, 150), pygame.Rect((i*variables.caseSize) - (variables.caseSize - 1), (j*variables.caseSize) - (variables.caseSize -1), (variables.caseSize - 1), (variables.caseSize - 1)))
+            pygame.draw.rect(ecran, (218, 227, 150), pygame.Rect((i*variables.caseSize) + 1, (j*variables.caseSize) + 1, (variables.caseSize - 1), (variables.caseSize - 1)))
             # else :
             #     pygame.draw.rect(ecran, (95, 97, 80), pygame.Rect((i*variables.caseSize) - (variables.caseSize - 1), (j*variables.caseSize) - (variables.caseSize -1), (variables.caseSize - 1), (variables.caseSize - 1)))
 
@@ -23,10 +23,10 @@ def mapInit() :
 # def mapDisplay() : 
 #     for x in map :
 #         for y in map[x] :
-#             if map[x][y] == 1 :
-#                 pygame.draw.rect(ecran, (95, 97, 80), pygame.Rect((x*variables.caseSize) - (variables.caseSize - 1), (y*variables.caseSize) - (variables.caseSize -1), (variables.caseSize - 1), (variables.caseSize - 1)))
+# 			if map[x][y] == 1 :
+# 				pygame.draw.rect(ecran, (95, 97, 80), pygame.Rect((i*variables.caseSize) + 1, (j*variables.caseSize) + 1, (variables.caseSize - 1), (variables.caseSize - 1)))
 #             else :
-#                 pygame.draw.rect(ecran, (218, 227, 150), pygame.Rect((x*variables.caseSize) - (variables.caseSize - 1), (y*variables.caseSize) - (variables.caseSize -1), (variables.caseSize - 1), (variables.caseSize - 1)))
+#                 pygame.draw.rect(ecran, (218, 227, 150), pygame.Rect((i*variables.caseSize) + 1, (j*variables.caseSize) + 1, (variables.caseSize - 1), (variables.caseSize - 1)))
                 
         
 
